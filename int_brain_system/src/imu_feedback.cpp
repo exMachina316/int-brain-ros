cb946bb18cab3f2ec596a1f603077c5b578f481a
Kostubh Khandelwal <khandelwalkostubh@gmail.com>
2025-08-12 01:49:59 +0530

    Update: Better response. removed gravity effect

--- int_brain_system/src/imu_feedback.cpp
+++ int_brain_system/src/imu_feedback.cpp
@@ -18,27 +18,38 @@ public:
 
         subscription_ = this->create_subscription<sensor_msgs::msg::Imu>(
             "/imu_sensor_broadcaster/imu", 10, std::bind(&ImuFeedbackNode::imu_callback, this, std::placeholders::_1));
-        
+
         feedback_pub_ = this->create_publisher<sensor_msgs::msg::JoyFeedback>("/joy/set_feedback", 10);
     }
 
 private:
     void imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg)
     {
-        // Use linear acceleration in x and y to calculate magnitude
-        float x_accel = msg->linear_acceleration.x;
-        float y_accel = msg->linear_acceleration.y;
+        // Use orientation to estimate gravity vector
+        double qx = msg->orientation.x;
+        double qy = msg->orientation.y;
+        double qz = msg->orientation.z;
+        double qw = msg->orientation.w;
 
+        // Components of gravity vector in sensor frame
+        // http://www.ros.org/reps/rep-0103.html#axis-orientation
+        // The gravity vector in the world frame is (0, 0, -g)
+        // We rotate it to the sensor frame using the orientation quaternion
+        double g = 9.81;
+        double gravity_x = 2 * (qx * qz - qw * qy) * g;
+        double gravity_y = 2 * (qy * qz + qw * qx) * g;
+
+        // Remove gravity from linear acceleration
+        float x_accel = msg->linear_acceleration.x - gravity_x;
+        float y_accel = msg->linear_acceleration.y - gravity_y;
+
+        // Use linear acceleration in x and y to calculate magnitude
         float magnitude = std::sqrt(x_accel * x_accel + y_accel * y_accel);
 
         float intensity = 1.0f / (1.0f + std::exp(-alpha_ * (magnitude - mid_point_)));
 
-        // Clamp t to the range [0, 1]
-        if (intensity < 0.0f) {
-            intensity = 0.0f;
-        } else if (intensity > 1.0f) {
-            intensity = 1.0f;
-        }
+        // Clamp intensity to the range [0, 1]
+        intensity = std::max(0.0f, std::min(1.0f, intensity));
 
         auto feedback_msg = std::make_unique<sensor_msgs::msg::JoyFeedback>();
         feedback_msg->type = sensor_msgs::msg::JoyFeedback::TYPE_RUMBLE;
